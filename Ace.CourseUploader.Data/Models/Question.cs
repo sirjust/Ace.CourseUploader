bcf335d9db2b55f4b785fa79b15f76f08358d1ac
sirjust <theoperaking@yahoo.com>
2022-02-03 08:29:20 -0800

    Get questions from spreadsheet, fix true/false

--- Ace.CourseUploader.Data/Models/Question.cs
+++ Ace.CourseUploader.Data/Models/Question.cs
@@ -5,7 +5,18 @@ namespace Ace.CourseUploader.Data.Models
     public class Question
     {
         public string QuizId { get; set; }
-        public string Text { get; set; }
         public List<Answer> Answers { get; set; }
+        public string CourseName { get; set; }
+        public string QuizName { get; set; }
+        public int QuestionNumber { get; set; }
+        public int PassPercentage { get; set; }
+        public string QuestionText { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+        public string Answer4 { get; set; }
+        public string Answer5 { get; set; }
+        public string CorrectAnswer { get; set; }
+        public string QuizQuestionImage { get; set; }
     }
 }
--- Ace.CourseUploader.Data/SpreadsheetReader.cs
+++ Ace.CourseUploader.Data/SpreadsheetReader.cs
@@ -1,6 +1,8 @@
 ﻿using Ace.CourseUploader.Data.Models;
 using ExcelToEnumerable;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Ace.CourseUploader.Data
 {
@@ -11,14 +13,54 @@ namespace Ace.CourseUploader.Data
             Console.WriteLine("Reading spreadsheet", filePath);
             Console.WriteLine();
 
-            var result = filePath.ExcelToEnumerable<Course>(
+            var courses = GetCourses(filePath);
+            var questions = GetRawQuestionData(filePath);
+            SetTrueFalse(questions);
+        }
+
+        private static List<Course> GetCourses(string filePath)
+        {
+            return filePath.ExcelToEnumerable<Course>(
             x => x
             .Property(y => y.CourseName).UsesColumnNamed("Course Name")
             .Property(y => y.ProductName).UsesColumnNamed("Product Name")
             .Property(y => y.StudentQuizFileName).UsesColumnNamed("Student Quiz File Name")
             .Property(y => y.Category).Ignore()
             .Property(y => y.Author).Ignore()
-            );
+            ).ToList();
+        }
+
+        private static List<Question> GetRawQuestionData(string filePath)
+        {
+            return filePath.ExcelToEnumerable<Question>(
+            x => x.UsingSheet("Quiz_Questions FOR TESTING ONLY")
+            
+            .Property(y => y.CourseName).UsesColumnNamed("Course Name")
+            .Property(y => y.QuizName).UsesColumnNamed("Quiz Name")
+            .Property(y => y.QuestionNumber).UsesColumnNamed("Question Number")
+            .Property(y => y.PassPercentage).UsesColumnNamed("Pass Percentage")
+            .Property(y => y.QuestionText).UsesColumnNamed("Question")
+            .Property(y => y.Answer1).UsesColumnNamed("Answer 1")
+            .Property(y => y.Answer2).UsesColumnNamed("Answer 2")
+            .Property(y => y.Answer3).UsesColumnNamed("Answer 3")
+            .Property(y => y.Answer4).UsesColumnNamed("Answer 4")
+            .Property(y => y.Answer5).UsesColumnNamed("Answer 5")
+            .Property(y => y.CorrectAnswer).UsesColumnNamed("Correct Answer")
+            .Property(y => y.QuizQuestionImage).UsesColumnNamed("Quiz Question Image")
+            .Property(y => y.QuizId).Ignore()
+            ).ToList();
+        }
+
+        private void SetTrueFalse(List<Question> questions)
+        {
+            foreach(var question in questions)
+            {
+                if(question.Answer1 == "1" && question.Answer2 == "0" && question.Answer3 == null && question.Answer4 == null && question.Answer5 == null)
+                {
+                    question.Answer1 = "TRUE";
+                    question.Answer2 = "FALSE";
+                }
+            }
         }
     }
 }
